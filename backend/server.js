c316f2aadc32aaeb945959917b3276a285c03d7b
jlbur <jlburks1500@gmail.com>
2023-09-14 23:47:38 -0500

    dev dependency 'vite' added

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("succesful response");
 });
 
 app.listen(PORT, () => console.log("running on port 3000"));
