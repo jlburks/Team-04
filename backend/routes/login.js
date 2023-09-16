ea7f0b6623f303a939eb062326e8c899bdf6309a
jlbur <jlburks1500@gmail.com>
2023-09-15 23:12:01 -0500

    Useful error handling in login implementation

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -17,8 +17,15 @@ Route.post("/", (req, res) => {
     `SELECT * FROM users WHERE username = '${username}'`,
     (e, user) => {
       if (e) {
-        console.log("ERROR ", e.sqlMessage);
-        res.status(500).json({ error: "Internal Server Error" });
+        console.log("ERROR ", e);
+        return res
+          .status(500)
+          .json({ login: false, error: "Internal Server Error" });
+      } else if (user.length === 0) {
+        console.log("no results found", user);
+        return res
+          .status(500)
+          .json({ login: false, error: "username not found" });
       }
       bcrypt.compare(password, user[0].password).then((result) => {
         if (result === true) {
@@ -30,7 +37,7 @@ Route.post("/", (req, res) => {
             secret,
             { expiresIn: "1h" }
           );
-          console.log(token);
+
           return res.status(200).json({ login: true, token: token });
         }
         return res
