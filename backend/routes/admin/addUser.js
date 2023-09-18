8bc78f4fbc509a1de7a66ea1b0ead48b13937da6
jlbur <jlburks1500@gmail.com>
2023-09-17 19:50:38 -0500

    verification functions have been created

--- backend/routes/admin/addUser.js
+++ backend/routes/admin/addUser.js
@@ -3,21 +3,24 @@ const Route = express.Router();
 const bcrypt = require("bcrypt");
 
 const connection = require("../../connection");
+const adminFunc = require("./functions/verifyAdmin");
 
 const saltRounds = 10;
 
-Route.post("/addUser", (req, res) => {
-  const { username, password, role } = req.body;
-  bcrypt.genSalt(saltRounds, (e, salt) => {
-    if (e) return console.log(e);
-    bcrypt.hash(password, salt, function (e, hash) {
-      if (e) return console.log("password hash operation unsuccesful");
-      connection.query(
-        `INSERT INTO users (username,password,role) VALUES ('${username}','${hash}','${role}') `
-      );
-      res.status(201).json({ status: "new user created" });
-    });
-  });
+// add middleware to only allow admin to preform this action
+Route.post("/addUser", adminFunc.verifyAdmin, (req, res) => {
+  return res.json({ mission: "sucessful" });
+  // const { username, password, role } = req.body;
+  // bcrypt.genSalt(saltRounds, (e, salt) => {
+  //   if (e) return console.log(e);
+  //   bcrypt.hash(password, salt, function (e, hash) {
+  //     if (e) return console.log("password hash operation unsuccesful");
+  //     connection.query(
+  //       `INSERT INTO users (username,password,role) VALUES ('${username}','${hash}','${role}') `
+  //     );
+  //     res.status(201).json({ status: "new user created" });
+  //   });
+  // });
 });
 
 module.exports = Route;
